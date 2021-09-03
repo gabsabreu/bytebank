c12287f34549fbb7e40add84834e43a8904d9e33
Gabe Abreu de Araujo <g.abreu958@gmail.com>
2021-09-03 09:50:34 -0300

    adicionando lista e barra de navegação inferior

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,24 +1,49 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Column(
-      children: const <Widget>[
-        Text(
-          'Deliver features faster',
-          textDirection: TextDirection.ltr,
-        ),
-        Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
-        ),
-        Expanded(
-          child: FittedBox(
-            fit: BoxFit.contain, // otherwise the logo will be tiny
-            child: FlutterLogo(),
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          body: Column(
+            children: <Widget>[
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('100.0'),
+                  subtitle: Text('1000'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('200.0'),
+                  subtitle: Text('2000'),
+                ),
+              ),
+            ],
+          ),
+          appBar: AppBar(
+            title: Text('Transferências'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.add),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.business),
+                label: 'Business',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.school),
+                label: 'School',
+              ),
+            ],
           ),
         ),
-        Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
-        ),
-      ],
-    ));
+      ),
+    );
