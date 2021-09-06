fea0219a25038e40ed225daa32a4cca30be129ff
Gabe Abreu de Araujo <g.abreu958@gmail.com>
2021-09-06 15:43:44 -0300

    refactor class editor

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -26,31 +26,10 @@ class FormularioTransferencia extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                controller: _controladorCampoNumeroConta,
-                style: TextStyle(fontSize: 24.0),
-                decoration: InputDecoration(
-                  labelText: 'Número da conta',
-                  hintText: '0000',
-                ),
-                keyboardType: TextInputType.number,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                controller: _controladorCampoValor,
-                style: TextStyle(fontSize: 24.0),
-                decoration: InputDecoration(
-                  icon: Icon(Icons.monetization_on),
-                  labelText: 'Valor',
-                  hintText: '0.00',
-                ),
-                keyboardType: TextInputType.number,
-              ),
-            ),
+            Editor(_controladorCampoNumeroConta, 'Numero da conta', '0000',
+                Icons.home_work_outlined),
+            Editor(
+                _controladorCampoValor, 'Valor', '0.00', Icons.monetization_on),
             ElevatedButton(
               child: const Text('Confirmar'),
               onPressed: () {
@@ -71,6 +50,32 @@ class FormularioTransferencia extends StatelessWidget {
             )
           ],
         ));
+  }
+}
+
+class Editor extends StatelessWidget {
+  final TextEditingController _controlador;
+  final String _rotulo;
+  final String _dica;
+  final IconData _icone;
+
+  Editor(this._controlador, this._rotulo, this._dica, this._icone);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: _controlador,
+        style: TextStyle(fontSize: 24.0),
+        decoration: InputDecoration(
+          icon: Icon(_icone),
+          labelText: _rotulo,
+          hintText: _dica,
+        ),
+        keyboardType: TextInputType.number,
+      ),
+    );
   }
 }
 
